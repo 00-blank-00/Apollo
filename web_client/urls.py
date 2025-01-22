0978988cc30b7d6195ed190c6f8d0c54385f6583
__blank__ <63240876+00-blank-00@users.noreply.github.com>
2025-01-22 20:05:56 +0100

    updated_view

--- web_client/urls.py
+++ web_client/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.main),
     path('database_viewer/', views.database_viewer),
     path('database_viewer/text_document/<int:file_id>', views.text_doc_viewer),
-    path('admin/', admin.site.urls)
 ]
--- web_client/views.py
+++ web_client/views.py
@@ -16,25 +16,3 @@ def database_viewer(request: HttpRequest):
 def text_doc_viewer(request: HttpRequest, file_id):
   file = TextEntry.objects.get(id = file_id)
   return HttpResponse(render(request, 'text_doc_viewer.html', {"textEntry": file}))
-
-def user_login(request: HttpRequest):
-  if request.method == "POST":
-    print("Invalid password!")
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-
-    if not User.objects.filter(username = username).exists():
-      messages.error(request, 'Invalid Username')
-      return redirect('/user_login/')
-    
-    user = authenticate(username = username, password = password)
-
-    if user is None:
-      messages.error(request, "Invalid Password")
-      
-      return redirect('/user_login/')
-    else:
-      login(request, user)
-      return redirect('')
-  else:
-    return render(request, 'user_login.html')

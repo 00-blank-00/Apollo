dacbd4378afe69b06784b69b2d5e31bbe3436ea5
__blank__ <63240876+00-blank-00@users.noreply.github.com>
2025-01-22 19:58:27 +0100

    dev3

--- web_client/views.py
+++ web_client/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import TextEntry
 
 def main(request: HttpRequest):
-  return HttpResponse(render(request, 'main.html'))
+  return HttpResponse(render(request, 'main.html', {"username": request.user.get_username()}))
 
 def database_viewer(request: HttpRequest):
   files = TextEntry.objects.all().values()
@@ -12,3 +16,25 @@ def database_viewer(request: HttpRequest):
 def text_doc_viewer(request: HttpRequest, file_id):
   file = TextEntry.objects.get(id = file_id)
   return HttpResponse(render(request, 'text_doc_viewer.html', {"textEntry": file}))
+
+def user_login(request: HttpRequest):
+  if request.method == "POST":
+    print("Invalid password!")
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    if not User.objects.filter(username = username).exists():
+      messages.error(request, 'Invalid Username')
+      return redirect('/user_login/')
+    
+    user = authenticate(username = username, password = password)
+
+    if user is None:
+      messages.error(request, "Invalid Password")
+      
+      return redirect('/user_login/')
+    else:
+      login(request, user)
+      return redirect('')
+  else:
+    return render(request, 'user_login.html')
